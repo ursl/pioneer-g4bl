f9d5a471f22a421b11efed7efdf91b43fbecc410
Urs Langenegger <urslangenegger@gmail.com>
2022-04-07 08:58:47 +0200

    parses CALO?NTR.txt and fills tree

---
+++ macros/anaTracks.C
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+
+//----------------------------------------------------------------------
+TTree* fillTree(string filename) {
+  ifstream INS;
+  vector<string> vlines; 
+  string sline;
+  INS.open(filename);
+  while (getline(INS, sline)) {
+    vlines.push_back(sline);
+  }
+
+  // -- parse beginning to set up tree
+  string tname = vlines[0].substr(1); 
+  string dname = tname.substr(tname.find(" ")+1); 
+
+  vector<string> vars;
+  map<string, double*> varDouble;
+  map<string, int*> varInt;
+
+  istringstream istring(vlines[1].substr(1)); 
+  istring >> sline; 
+  cout << "adding ";
+  while (!istring.fail()) {
+    cout << sline << " "; 
+    vars.push_back(sline);
+    if ((string::npos != sline.find("PDGid"))
+        || (string::npos != sline.find("EventID"))
+        || (string::npos != sline.find("TrackID"))
+        || (string::npos != sline.find("ParentID"))
+        ) {
+      varInt.insert(make_pair(sline, new int));
+    } else {
+      varDouble.insert(make_pair(sline, new double));
+    }
+    istring >> sline; 
+  }
+  cout << endl;
+
+  // -- set up tree  
+  TTree *t = new TTree(dname.c_str(), tname.c_str());
+  for (unsigned int i = 0; i < vars.size(); ++i) {
+    cout << "vars[" << i << "] = " << vars[i] << ": "; 
+    if (varDouble.end() != varDouble.find(vars[i])) {
+      cout << varDouble[vars[i]] << " (double)";
+      t->Branch(vars[i].c_str(), varDouble[vars[i]], Form("%s/D", vars[i].c_str()));
+    }
+    if (varInt.end() != varInt.find(vars[i])) {
+      cout << varInt[vars[i]] << " (int)";
+      t->Branch(vars[i].c_str(), varInt[vars[i]], Form("%s/I", vars[i].c_str()));
+    }
+    cout << endl;
+  }
+
+
+  // -- read rest of file and fill into tree
+  for (unsigned int il = 2; il < vlines.size(); ++il) {
+    cout << "line " << il << endl;
+    istringstream values(vlines[il]); 
+    for (unsigned int i = 0; i < vars.size(); ++i) {
+      if (varDouble.end() != varDouble.find(vars[i])) {
+        values >> *varDouble[vars[i]]; 
+        cout << "parsed " << vars[i] << " = " << *varDouble[vars[i]] << endl;
+      }
+      if (varInt.end() != varInt.find(vars[i])) {
+        values >> *varInt[vars[i]]; 
+        cout << "parsed " << vars[i] << " = " << *varInt[vars[i]] << endl;
+      }
+    }
+    t->Fill();
+  }
+  
+  return t; 
+}
+
+// ----------------------------------------------------------------------
+void anaTracks(string filename = "../pioneer/PionTransport/CALOCNTR.txt") {
+  TTree *t = fillTree(filename);
+  t->Print();
+
+  t->Draw("y:x", "", "colz");
+}
