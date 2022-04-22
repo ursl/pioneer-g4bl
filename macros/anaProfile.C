db6bdccdbfd7a46370d8333e1bf64611e6101d27
Urs Langenegger <urslangenegger@gmail.com>
2022-04-22 17:59:23 +0200

    attempts with ellipses

--- macros/anaProfile.C
+++ macros/anaProfile.C
@@ -11,6 +11,53 @@
 // ----------------------------------------------------------------------
 
 
+// general ellipse equation
+// ((x−x0)*cos(phi) + (y-y0))sin(phi))*((x−x0)*cos(phi) + (y-y0))sin(phi))/(a*a)
+// +((x−x0)*sin(phi) - (y-y0)cos(phi))*((x−x0)*sin(phi) - (y-y0)cos(phi))/(b*b)
+// = 1
+//
+// ellipse parameters in terms of sigmas and correlation
+// tan(2*phi) = (2*rho*sigmaX*sigmaY)/(sigmaX*sigmaX - sigmaY*sigmaY)
+// a          = ((cos2phi*sigmaX*sigmaX) - (sin2phi*sigmaY*sigmaY))/(cos2phi - sin2phi)
+// b          = ((cos2phi*sigmaY*sigmaY) - (sin2phi*sigmaX*sigmaX))/(cos2phi - sin2phi)
+
+
+
+// ----------------------------------------------------------------------
+void drawPS(double meanX, double meanY, double sigmaX, double sigmaY, double rho) {
+  TEllipse e; 
+  e.SetLineColor(kBlue); 
+  e.SetLineWidth(2); 
+  e.SetFillStyle(0); 
+
+  double phi     = 0.5*TMath::ATan((2*rho*sigmaX*sigmaY)/(sigmaX*sigmaX - sigmaY*sigmaY));
+  double phiDeg  = phi*TMath::RadToDeg();
+  cout << "phi = " << phiDeg << endl;
+  cout << "tan(2phi) = " << TMath::Tan(2.*phi) << endl;
+  cout << "calc:       " << (2.*rho*sigmaX*sigmaY)/(sigmaX*sigmaX - sigmaY*sigmaY) << endl;
+
+  double cos2phi = TMath::Cos(phi)*TMath::Cos(phi);
+  double sin2phi = TMath::Sin(phi)*TMath::Sin(phi);
+  double a       = ((cos2phi*sigmaX*sigmaX) - (sin2phi*sigmaY*sigmaY))/(cos2phi - sin2phi);
+  double b       = ((cos2phi*sigmaY*sigmaY) - (sin2phi*sigmaX*sigmaX))/(cos2phi - sin2phi);
+  a = TMath::Sqrt(a);
+  b = TMath::Sqrt(b);
+  e.DrawEllipse(meanX, meanY, a, b, 0., 360., phi);
+}
+
+
+// ----------------------------------------------------------------------
+void drawPS0(double mean, double meanPrime, double eps, double beta, double phi) {
+  TEllipse e; 
+  e.SetLineColor(kBlue); 
+  e.SetLineWidth(2); 
+  e.SetFillStyle(0); 
+  // test1(600, 2.5, 15)
+  //  e.DrawEllipse(mean, meanPrime, TMath::Sqrt(eps*beta), TMath::Sqrt(eps/beta), 0., 360., phi);
+  e.DrawEllipse(mean, meanPrime, eps, beta, 0., 360., phi);
+}
+
+
 
 //----------------------------------------------------------------------
 TTree* fillTree(string filename) {
@@ -97,4 +144,20 @@ void anaProfile(string filename = "profile.txt") {
   t->Print();
 
   t->Draw("meanX:Z");
+}
+
+
+// ----------------------------------------------------------------------
+void drawPad(double x = 100., double y = 50.) {
+
+  gStyle->SetOptStat(0);
+  
+  TH2D *h2 = new TH2D("h2", "", 100., -x, x, 100., -y, y);
+
+  h2->Draw("axis");
+  gPad->SetGridx(1);
+  gPad->SetGridy(1);
+
+  //  drawPS(-21.7, -13.0, eps, beta, phi);
+  
 }
