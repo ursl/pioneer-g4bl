ce4cfab30fd00165871428f19eaf12dbc39ef097
Urs Langenegger <urslangenegger@gmail.com>
2022-04-25 15:46:17 +0200

    added a hash version (much faster and factor 4 better RSS size for large files)

--- macros/runCheckDuplicateTracks.cc
+++ macros/runCheckDuplicateTracks.cc
@@ -4,30 +4,66 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <functional>
 #include <map>
 
+#include "TString.h"
 
 using namespace std;
 
+bool gVerbose(false);
+bool gHash(true);
+
+// -- memory hog:   map<string, int> 18Gb RSS for 69M lines
+//    next version: map<size_t, int> 4GB RSS for 69M lines with a hash
 // ----------------------------------------------------------------------
 void checkDuplicates(string filename) {
   map<string, int> allLines; 
+  map<size_t, int> allLinesHash; 
 
   ifstream INS;
   string sline;
   INS.open(filename);
+  long iLines(0), iDuplicates(0);
   while (getline(INS, sline)) {
-    if (allLines.find(sline) != allLines.end()) {
-      allLines[sline]++;
-      cout << "duplicate line: " << endl;
-      cout << sline << endl; 
+    if (gHash) {
+      allLinesHash[std::hash<std::string>{}(sline)]++;
     } else {
-      allLines.insert(make_pair(sline, 1));
+      allLines[sline]++;
+    }
+    ++iLines; 
+    if (0 == iLines%1000) {
+      if (gVerbose) {
+        cout << Form("line %10ld", iLines) << endl;
+      } else {
+        cout << "\r" << Form("line %10ld", iLines) << flush;
+      }
     }
   }
 
-  // FIXME print duplicate lines 
-
+  if (gHash) {
+    auto lastLine = allLinesHash.end();
+    for (auto it = allLinesHash.begin(); it != lastLine; ++it) {
+      if (it->second != 1) {
+        ++iDuplicates;
+        if (gVerbose) cout << it->first << " (" << it->second << ")" << endl;
+      }
+    }
+  } else {
+    auto lastLine = allLines.end();
+    for (auto it = allLines.begin(); it != lastLine; ++it) {
+      if (it->second != 1) {
+        ++iDuplicates;
+        if (gVerbose) cout << it->first << " (" << it->second << ")" << endl;
+      }
+    }
+  } 
+    
+  cout << endl;
+  cout << "Counted a total of " << iDuplicates << " lines that appeared at least twice among "
+       << iLines << " lines (fraction = " << static_cast<double>(iDuplicates)/iLines << ")"
+       << endl;
+  cout << "Used " << (gHash? "hash": "no hash") << " version" << endl;
 }
 
 
@@ -39,6 +75,8 @@ int main(int argc, char *argv[]) {
   // -- command line arguments
   for (int i = 0; i < argc; i++){           
     if (!strcmp(argv[i], "-f"))  {filename = argv[++i];} 
+    if (!strcmp(argv[i], "-h0")) {gHash = false;} 
+    if (!strcmp(argv[i], "-v"))  {gVerbose = true;} 
   }
   
   checkDuplicates(filename);
